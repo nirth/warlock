b17e85132ef93b1290160c802d25f0d13948c34d
David Sergey <nirth.furzahad@gmail.com>
2016-05-02 07:11:00 +0200

    Less complicated version of the middleware. Middleware will take in action, and use `setTimeout` to dispatch action after current stack is done (indicating timeout as 0). This could cause race conditions, so I need to keep an eye for that.

--- app/reducers/asyncStorageMiddleware.js
+++ app/reducers/asyncStorageMiddleware.js
@@ -2,16 +2,18 @@ import {AsyncStorage} from 'react-native';
 
 const nextStack = (fun) => setTimeout(fun, 0);
 
-const saveState = (store) => () => {
-  const state = store.getState();
-  AsyncStorage.setItem('warlockAppState', JSON.stringify(state));
-};
-
 export const asyncStorageMiddleware = (store) => (next) => (action) => {
   // Saving application state on every mutation.
-  nextStack(saveState(store));
-  // TODO: We probably need to ignore async actions.
-  // TODO: It might make sense to keep 5-10 versions of app state in key-value store.
-  // in case one of them is invalid use previous.
+
+  if (action.type === 'saveAppState') {
+    const state = store.getState();
+    // TODO: It might make sense to keep 5-10 versions of app state in key-value store.
+    // nextStach(saveState(store));
+    AsyncStorage.setItem('warlockAppState', JSON.stringify(state));
+    return null;
+  }
+  // TODO: Probably need to ignore async actions.
+  nextStack(() => store.dispatch({type: 'saveAppState'}));
+
   return next(action);
 };
