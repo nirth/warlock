e2e092481c453b97a77c05aac63d673c9c55b2d6
David Sergey <nirth.furzahad@gmail.com>
2016-05-03 07:13:32 +0200

    Adding more stuff to ritual item renderer

--- app/rituals/RitualItem.js
+++ app/rituals/RitualItem.js
@@ -1,20 +1,44 @@
+/**
+ * @flow
+ */
 import React, {
   PropTypes,
   Text,
   View,
 } from 'react-native';
-// import {styles} from '../styles';
+import {
+  Spacer,
+  Title,
+} from '../components';
+import {
+  ritualItemStyle,
+  ritualItemRowStyle,
+} from './styles';
 
-const RitualItem = ({name, uuid}) => (
-  <View>
-    <Text>{name}</Text>
-    <Text>{uuid}</Text>
+const formatDate = (epoch: Number) => (new Date(epoch)).toDateString();
+
+const RitualItem = ({name, uuid, createdAt, updatedAt}) => (
+  <View style={ritualItemStyle}>
+    <View style={ritualItemRowStyle}>
+      <Title>{name}</Title>
+    </View>
+    <View style={ritualItemRowStyle}>
+      <Text>{formatDate(createdAt)}</Text>
+      <Spacer />
+      <Text>{formatDate(updatedAt)}</Text>
+    </View>
+    <View style={ritualItemRowStyle}>
+      <Spacer />
+      <Text>{uuid}</Text>
+    </View>
   </View>
 );
 
 RitualItem.propTypes = {
   name: PropTypes.string.isRequired,
   uuid: PropTypes.string.isRequired,
+  createdAt: PropTypes.number.isRequired,
+  updatedAt: PropTypes.number.isRequired,
 };
 
 export default RitualItem;
