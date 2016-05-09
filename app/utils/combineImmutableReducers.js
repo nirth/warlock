9b8489c5f3489642bc0528ae752ed1e96f720f0a
David Sergey <nirth.furzahad@gmail.com>
2016-05-09 07:37:19 +0200

    Linted combineImmutableReducers.

--- app/utils/combineImmutableReducers.js
+++ app/utils/combineImmutableReducers.js
@@ -4,14 +4,11 @@ export const combineImmutableReducers = (reducers) => {
   console.warn('combineImmutableReducers', reducers);
   const keys = Object.keys(reducers);
 
-  return (state, action) => {
-    return Immutable(keys.reduce((currentState, reducerName) => {
-      console.warn('combineImmutableReducers', currentState, currentState.asMutable);
-      const reducer = reducers[reducerName];
-      const childState = currentState[reducerName];
-      const nextState = currentState;
-      nextState[reducerName] = reducer(childState, action);
-      return nextState;
-    }));
-  };
+  return (state, action) => Immutable(keys.reduce((currentState, reducerName) => {  
+    const reducer = reducers[reducerName];
+    const childState = currentState[reducerName];
+    const nextState = currentState;
+    nextState[reducerName] = reducer(childState, action);
+    return nextState;
+  }));
 };
