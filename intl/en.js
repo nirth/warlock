8611a997a658e73f5a3aa62b284bc86bbcefcd43
David Sergey <nirth.furzahad@gmail.com>
2016-05-14 16:41:30 +0200

    Attempting to add internationalisation

---
+++ intl/en.js
@@ -0,0 +1,7 @@
+export default {
+  meta: {
+    initializing: 'Warlock is preparing your workbench',
+    loading: 'Warlock is fetching your rituals',
+    noRituals: 'You don‘t have any rituals mortal! Create at least one!',
+  },
+};
