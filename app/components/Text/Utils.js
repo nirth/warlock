e8d08f0033cfd1353e71a43d7114f5634f6ca7a5
David Sergey <nirth.furzahad@gmail.com>
2016-05-14 13:01:34 +0200

    Implemented `@textField` decorator, that injects properties that can be used by text field classes to render and translate text

--- app/components/Text/Utils.js
+++ app/components/Text/Utils.js
@@ -1,4 +1,5 @@
-import React from 'react-native';
+/* @flow */
+import React, {PropTypes} from 'react-native';
 import {FormattedDate, FormattedTime} from 'react-intl';
 import {curry} from 'ramda';
 
@@ -14,4 +15,40 @@ export const resolveTextNode = (value, styles, date = false, time = false) => {
   }
 
   return createTextNode(styles, value);
+};
+
+const componentNameCollisions = (propTypes = {}, propNames = []) => (
+  Object.keys(propTypes).filter((key) => propNames.indexOf(key) !== -1)
+);
+
+const assignProps = (propTypes = {}, defaultProps = {}) => ({
+  propTypes: Object.assign({
+    date: PropTypes.bool,
+    time: PropTypes.bool,
+  }, propTypes),
+  defaultProps: Object.assign({
+    date: false,
+    time: false,
+  }, defaultProps),
+});
+
+/**
+ * Decorator
+ * Note: this function has a side effect, it modifies component directly.
+ *
+ *
+ *
+ */
+export const textField = (componentClass: Function) => {
+  const props = ['date', 'time'];
+  const {propTypes, defaultProps} = componentClass;
+  const collisions = componentNameCollisions(componentClass.propTypes, props);
+  
+  if (collisions.length > 0) {
+    const ns = 'components.text.utils.textField';
+    throw new Error(`${ns} Component ${componentClass} already has property(ies): ${collisions}`);
+  }
+  
+  Object.assign(componentClass, assignProps(propTypes, defaultProps));
+  return componentClass;
 };
