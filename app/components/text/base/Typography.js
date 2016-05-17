b1befbb41ed7503fc86cea3b5405fd707df4191d
David Sergey <nirth.furzahad@gmail.com>
2016-05-17 07:27:55 +0200

    Reorganising stylesheets

--- app/components/text/base/Typography.js
+++ app/components/text/base/Typography.js
@@ -1,36 +1,10 @@
 import {StyleSheet} from 'react-native';
 
-const TITLE_STYLE = {
-  fontSize: 20,
-};
-
-const SUBTITLE_STYLE = {
-  fontSize: 16,
-};
-
-const MESSAGE_STYLE = {
-  fontSize: 15,
-};
-
-const BORING_TEXT_STYLE = {
-  fontSize: 12,
-};
-
-const LIGHT_THEME = {
-  color: 'rgba(12, 18, 15, 1);',
-};
-
-const DARK_THEME = {
-  color: 'orange',
-};
-
 const Typography = StyleSheet.create({
-  title: TITLE_STYLE,
-  subtitle: SUBTITLE_STYLE,
-  message: MESSAGE_STYLE,
-  boringText: BORING_TEXT_STYLE,
-  lightTheme: LIGHT_THEME,
-  darkTheme: DARK_THEME,
+  title: {fontSize: 20},
+  subtitle: {fontSize: 16},
+  message: {fontSize: 15},
+  boringText: {fontSize: 12},
 });
 
 export default Typography;
